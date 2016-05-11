054d1f19134cdac5b63ba09d6287cc9a668548ba
agdavid <antonio.g.david@gmail.com>
2016-05-11 15:07:32 -0400

    complete recipe CRUD

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,16 +1,21 @@
 class RecipesController < ApplicationController
+
+  def index
+    @recipes = Recipe.all
+  end
+
   def show
     @recipe = Recipe.find(params[:id])
   end
 
   def new
-    @recipe = Recipe.new
+    @recipe = current_user.recipes.build
   end
 
   def create
-    @recipe = Recipe.new(recipe_params)
+    @recipe = current_user.recipes.build(recipe_params)
     if @recipe.save
-      redirect_to recipe_path(@recipe)
+      redirect_to recipe_path(@recipe), :notice => "Recipe successfully created."
     else
       render "new"
     end
@@ -23,10 +28,16 @@ class RecipesController < ApplicationController
   def update
     @recipe = Recipe.find(params[:id])    
     if @recipe.update(recipe_params)
-      redirect_to recipe_path(@recipe)
+      redirect_to recipe_path(@recipe), :notice => "Recipe successfully updated."
     else
       render "edit"
     end
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    redirect_to root_path, :notice => "Recipe successfully deleted."
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'welcome#home'
+  root 'recipes#index'
 
   devise_for :users, 
               path_names: { sign_in: 'login', sign_out: 'logout'},
