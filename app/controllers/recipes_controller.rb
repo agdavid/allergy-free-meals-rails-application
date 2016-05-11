d75ffdd1861ac2f1bdc6ac5c100c0a361c884c2d
agdavid <antonio.g.david@gmail.com>
2016-05-10 21:05:03 -0400

    add flash and error messages

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,7 @@
 class RecipesController < ApplicationController
+  before_action :set_recipe, only: [:show, :edit, :update]
 
   def show
-    @recipe = Recipe.find(params[:id])
   end
 
   def new
@@ -11,26 +11,28 @@ class RecipesController < ApplicationController
   def create
     @recipe = Recipe.new(recipe_params)
     if @recipe.save
-      redirect_to recipe_path(@recipe)
+      redirect_to recipe_path(@recipe), notice: "Recipe was successfully created."
     else
       render "new"
     end
   end
 
   def edit
-    @recipe = Recipe.find(params[:id])
   end
 
-  def update
-    @recipe = Recipe.find(params[:id])    
+  def update   
     if @recipe.update(recipe_params)
-      redirect_to recipe_path(@recipe)
+      redirect_to recipe_path(@recipe), notice: "Recipe was successfully updated."
     else
       render "edit"
     end
   end
 
   private
+    def set_recipe
+      @recipe = Recipe.find(params[:id])
+    end
+    
     def recipe_params
       params.require(:recipe).permit(:title, :description, :instructions,
         :ingredients_attributes => [:id, :amount, :_destroy, :item_attributes => [:id, :_destroy, :name]])
