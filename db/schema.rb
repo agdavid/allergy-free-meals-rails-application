3af3668b556702c3f65c7b200790c96346f21afb
agdavid <antonio.g.david@gmail.com>
2016-05-10 21:07:17 -0400

    migrations for allergens and recipe_allergens

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160511004206) do
+ActiveRecord::Schema.define(version: 20160511010608) do
+
+  create_table "allergens", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "ingredients", force: :cascade do |t|
     t.string   "amount"
@@ -25,6 +31,13 @@ ActiveRecord::Schema.define(version: 20160511004206) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "recipe_allergens", force: :cascade do |t|
+    t.integer  "allergen_id"
+    t.integer  "recipe_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "recipes", force: :cascade do |t|
