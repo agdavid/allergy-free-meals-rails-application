a25ab418b83f1343094bf841df64dd1dc38b8408
agdavid <antonio.g.david@gmail.com>
2016-05-17 10:30:20 -0400

    add paperclip image to allergen model and migration

--- app/models/allergen.rb
+++ app/models/allergen.rb
@@ -4,4 +4,7 @@ class Allergen < ActiveRecord::Base
 
   has_many :user_allergens
   has_many :users, through: :user_allergens
+
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>", icon: "67x67>" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
