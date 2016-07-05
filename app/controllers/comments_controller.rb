caf80e89f3166a245054ebf7bb08797ef72d39fc
agdavid <antonio.g.david@gmail.com>
2016-07-05 16:39:53 -0400

    refactor comments#show to respond_to both HTML and JSON

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -13,6 +13,10 @@ class CommentsController < ApplicationController
 
   def show
     @comment = Comment.find(params[:id])
+    respond_to do |format|
+      format.html { render :show }
+      format.json { render json: @comment }
+    end
   end
 
   private
