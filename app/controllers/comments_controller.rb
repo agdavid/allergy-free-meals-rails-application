74d56788e56c5b6d51edc1acd1b490d58019244e
agdavid <antonio.g.david@gmail.com>
2016-07-05 16:25:25 -0400

    recheck and confirm comments#create and comments#show action and views

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,23 @@
 class CommentsController < ApplicationController
 
   def create
-    raise params.inspect
+    # "comment"=>{"description"=>"This is a test comment", "user_id"=>"1", "recipe_id"=>"2"}
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      flash[:success] = "Comment successfully created."
+      redirect_to recipe_comment_path(@comment.recipe, @comment)
+    else
+      render "recipes/show"
+    end
   end
+
+  def show
+    @comment = Comment.find(params[:id])
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:description, :user_id, :recipe_id)
+    end
 
 end
