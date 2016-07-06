20f6bc61baa6cf19af844a3c7ed2b3cc309be673
agdavid <antonio.g.david@gmail.com>
2016-07-06 08:01:49 -0400

    add JS to preventDefault load of comments#index

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,6 +1,13 @@
 $(function () {
-  $(".js-showNewComment").on("click", function(event) {
-    alert("I did not render the show page!");
+  $("a.js-loadComments").on("click", function(event) {
+    // Step 1. Hijack the link to load all comments
+    alert("You clicked the Show All Comments link!")
     event.preventDefault();
+    // var newCommentText = $('#newCommentDescription').val();
+    // var new_comment_html = "";
+
+    // debugger;
+    // $('.new-comment-show').toggle("hide");
+    // //function NewComment(id, description, user, recipe)
   });
 });
