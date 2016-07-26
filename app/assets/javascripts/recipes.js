233b20579930fad442f0d2bbba5706fbefaee2b6
agdavid <antonio.g.david@gmail.com>
2016-07-26 14:31:07 -0400

    iterate over JSON user recipes, concat string and insert into DOM

--- app/assets/javascripts/recipes.js
+++ app/assets/javascripts/recipes.js
@@ -1,21 +1,30 @@
 $(function() {
-  $("#js-showUserRecipes").on("click", function(click) {
+  // render user_recipes index page via jQuery and an Active Model Serialization JSON Backend
+  $(".js-showUserRecipes").on("click", function(click) {
+    // render response without page refresh
     click.preventDefault();
     var userId = parseInt($(click['target']).attr("data-id"));
     var recipes_html = ""
 
+    // translate JSON into JS Model Object
     function User(id, name, recipes) {
       this.id = id
       this.name = name
       this.recipes = recipes
+      // method on the prototype
       this.display_each_recipe = function() {
-        alert("You are in the user prototype method!");
+        // reveal has_many relationship of prototype
+        $.each(this.recipes, function(i, recipe) {
+          recipes_html = recipes_html.concat("<li><a href='/users/" + userId + "/recipes/" + recipe.id + "'>" + recipe.title + "</a></li>") // TO MAKE STRING OF RECIPES and LINKS
+        });
       };
     };
 
+    // get JSON
     $.get("/users/" + userId + ".json", function(data) {  
       var user = new User(data['id'], data['name'], data['recipes']);
-      user.display_each_recipe();  
+      user.display_each_recipe();
+      $('.userRecipes').html(recipes_html);  
     });
 
   });
