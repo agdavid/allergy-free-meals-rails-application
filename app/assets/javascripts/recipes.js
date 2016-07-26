2a12bb8eaa52f68e2b4466695ea61d78cb972e2d
agdavid <antonio.g.david@gmail.com>
2016-07-26 13:58:07 -0400

    stub out the User prototype method #display_each_recipe

--- app/assets/javascripts/recipes.js
+++ app/assets/javascripts/recipes.js
@@ -2,16 +2,20 @@ $(function() {
   $("#js-showUserRecipes").on("click", function(click) {
     click.preventDefault();
     var userId = parseInt($(click['target']).attr("data-id"));
+    var recipes_html = ""
 
     function User(id, name, recipes) {
       this.id = id
       this.name = name
       this.recipes = recipes
+      this.display_each_recipe = function() {
+        alert("You are in the user prototype method!");
+      };
     };
 
     $.get("/users/" + userId + ".json", function(data) {  
       var user = new User(data['id'], data['name'], data['recipes']);
-      debugger;  
+      user.display_each_recipe();  
     });
 
   });
