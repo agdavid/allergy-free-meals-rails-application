da91d4ef67c0b0be5fd532f8d4286c15fabbd036
agdavid <antonio.g.david@gmail.com>
2016-09-22 15:23:58 -0400

    refactor GET comments to simplify AJAX type of 'post'

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,41 +1,7 @@
-// JS to load comments - replaced by Remote True
-// // Step 1: use jQuery document ready to load JS only when page loaded
-// $(function () {
-//   $("a.js-loadComments").on("click", function(event) {
-//     // Step 2: Hijack the link to load all comments
-//     // alert("You clicked the Show All Comments link!")
-//     event.preventDefault();
-//     // Step 3a: Fire low-level AJAX, get a response, load into DOM
-//         // $.ajax({
-//         //     method: "GET",
-//         //     //url of "this", which is the href attribute of "Show All Comments" link
-//         //     url: this.href, 
-//         // }).success(function(response) {
-//         //     //inject the plain comments#index response into DOM
-//         //     $("div.comments-index").html(response);    
-//         // });
-//     // Step 3b: Fire higher-level AJAX, get a response, load into DOM
-//         // $.get(this.href).success(function(response) {
-//         //     $("div.comments-index").html(response);    
-//         // });
-//     // Step 3c: Fire AJAX and receive JSON response instead of HTML
-//         // $.getJSON(this.href).success(function(json) {
-//         //     var $ol = $("div.comments-index ol");
-//         //     $ol.html(""); //clear the div for comments
-//         //     json.forEach(function(comment) { //iterate over each json comment in array
-//         //         //append each comment as <li> to the <ol>
-//         //         $ol.append("<li>" + comment.description + "</li>");
-//         //     });
-//         // });
-//     // Step 3d: Remote True paradigm for server-side AJAX
-//         // $.ajax({
-//         //     url: this.href,
-//         //     dataType: "script",
-//         // });
-//   });
-// });
 
 $(function() {
+
+  // 'Show All Comments' link
   $('.js-loadComments').on('click', function(event) {
     event.preventDefault();
     var recipeId = $(event.target).data('id');
@@ -46,12 +12,6 @@ $(function() {
       this.title = title
       this.user = user
       this.comments = comments
-      // this.display_each_comment = function() {
-      //   $.each(this.comments, function(i, comment) {
-      //     comments_html = comments_html.concat("<li><a href='/recipes/" + recipeId + "/comments/" + comment.id + "' class='js-showComment' recipe-id='" + recipeId + "' comment-id='" + comment.id + "'>" + comment.description + "</a></li>")
-      //   });
-      //   comments_html = comments_html.concat('</ol>');
-      // };
     };
 
     Recipe.prototype.display_each_comment = function() {
@@ -68,19 +28,12 @@ $(function() {
     });
   });
 
+  // 'Create Comment' button
   $('#new-comment-form').on("submit", function(event) {
     event.preventDefault();
-      // data = {
-      //   'authenticity_token': $("input[name='authenticity_token']").val(),
-      //   'comment': {
-      //     'description': $("input[name='comment[description]']").val(),
-      //     'user_id': $("input[name='comment[user_id]']").val(),
-      //     'recipe_id': $("input[name='comment[recipe_id]']").val()
-      //   }
-      // };
+
     $.ajax({
-      // method: 'POST'
-      type: ($("input[name='_method']").val()  || this.method),
+      type: 'post', //($("input[name='_method']").val()  || this.method),
       url: this.action,
       data: $(this).serialize(),
       success: function() {
@@ -90,6 +43,7 @@ $(function() {
     });
   });
 
+  // '[Comment]' show link
   $('.comments-index').on('click', '.js-showComment', function(event) {
     event.preventDefault();
     var recipeId = $(event.target).attr('recipe-id');
