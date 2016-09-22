238a6ac3b92f2fe0574a0efadd471659a6551e28
agdavid <antonio.g.david@gmail.com>
2016-09-22 15:46:30 -0400

    remove empty files

--- app/assets/javascripts/recipes.js
+++ app/assets/javascripts/recipes.js
@@ -13,11 +13,13 @@ $(function() {
 
     User.prototype.display_each_recipe = function() {
         $.each(this.recipes, function(i, recipe) {
+          debugger;
           recipes_html = recipes_html.concat("<li><a href='/users/" + userId + "/recipes/" + recipe.id + "'>" + recipe.title + "</a></li>");
         });
       };
 
-    $.get("/users/" + userId + ".json", function(data) {  
+    $.get("/users/" + userId + ".json", function(data) {
+      debugger;  
       var user = new User(data['id'], data['name'], data['recipes']);
       user.display_each_recipe();
       $('.userRecipes').html(recipes_html);  
